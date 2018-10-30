84288d3a67084bb1e3579dbeffb389c430df4dc6
Ivan Shamatov <status.enable@gmail.com>
2018-10-30 16:41:45 +0300

    initial commit

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,15 @@
+//= require home/jquery-3.2.1.min.js
+//= require home/propper.js
+//= require home/bootstrap.min.js
+
+//= require home/bootstrap-select.min.js
+//= require home/wow.min.js
+//= require home/jquery.parallax-scroll.js
+//= require home/owl.carousel.min.js
+//= require home/jquery.nice-select.min.js
+//= require home/imagesloaded.pkgd.min.js
+//= require home/isotope-min.js
+//= require home/jquery.magnific-popup.min.js
+
+//= require home/plugins.js
+//= require home/main.js
